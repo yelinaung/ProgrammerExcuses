8f0982c08b7017e53117455ec4ff0d3d13c7dc78
Ye Lin Aung <me@yelinaung.com>
2014-12-06 08:34:19 +0630

    move back to normal directory

--- app/src/main/java/com/yelinaung/programmerexcuses/Settings.java
+++ app/src/main/java/com/yelinaung/programmerexcuses/Settings.java
@@ -29,11 +29,6 @@ public class Settings extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
